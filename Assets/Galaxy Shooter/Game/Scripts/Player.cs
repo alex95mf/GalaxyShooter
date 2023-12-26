6022a9bfd80f1df985fff788a4abd115be6d8862
Alexander Martinez <alex95mf@gmail.com>
2023-12-26 17:11:49 -0500

    NEW: Disparo Laser

--- Assets/Galaxy Shooter/Game/Scripts/Player.cs
+++ Assets/Galaxy Shooter/Game/Scripts/Player.cs
@@ -5,80 +5,97 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Variables
+    [SerializeField]
+    private float _velocidad = 10.0f;
+    [SerializeField]
+    private Vector3 _PosicionInicial = new(0, -3.25f, 0);
+    [SerializeField]
+    private float _horizontalInput;
+    [SerializeField]
+    private float _verticalInput;
+    [SerializeField]
+    private GameObject _LaserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.25f;
+    [SerializeField]
+    private float _nextFire = 0.0f;
 
-    [SerializeField]
-    private float velocidad = 10.0f;
-    [SerializeField]
-    private Vector3 PosicionInicial = new(0, -3.25f, 0);
-    [SerializeField]
-    private float horizontalInput;
-    [SerializeField]
-    private float verticalInput;
-
-    // Start is called before the first frame update
     void Start()
     {
         // Posicionar Nave en Posicion Inicial
-        transform.position = PosicionInicial;
+        transform.position = _PosicionInicial;
     }
 
     void Update()
     {
         Movement();
+        Shoot();
     }
 
-    // Update is called once per frame
+    private void Shoot()
+    {
+        // Si presiono tecla espacio o click izquierdo del mouse
+        // Crear laser arriba de la posicion del Player
+        if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Mouse0)) && (Time.time > _nextFire))
+        {
+            // Cooldown
+            _nextFire = Time.time + _fireRate;
+            // Crear mi laser
+            Instantiate(_LaserPrefab, transform.position + new Vector3(0, 0.88f, 0), Quaternion.identity);
+        }
+    }
+
     void Movement()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
         int moverHorizontal = 0;
         int moverVertical = 0;
 
-        if (horizontalInput > 0.15)
+        if (_horizontalInput > 0.15)
             moverHorizontal = 1;
-        if (horizontalInput < -0.15)
+        if (_horizontalInput < -0.15)
             moverHorizontal = -1;
-        if (horizontalInput == 0)
+        if (_horizontalInput == 0)
             moverHorizontal = 0;
 
-        if (verticalInput > 0.15)
+        if (_verticalInput > 0.15)
             moverVertical = 1;
-        if (verticalInput < -0.15)
+        if (_verticalInput < -0.15)
             moverVertical = -1;
-        if (verticalInput == 0)
+        if (_verticalInput == 0)
             moverVertical = 0;
 
         // Control normal
         // Eje X
         if (transform.position.x >= -7.5 && transform.position.x <= 7.5 )
         {
-            transform.Translate(Vector3.right * Time.deltaTime * velocidad * moverHorizontal);
+            transform.Translate(Vector3.right * Time.deltaTime * _velocidad * moverHorizontal);
         }
         // Eje Y
         if (transform.position.y >= -4.25f && transform.position.y <= 1.25f)
         {
-            transform.Translate(Vector3.up * Time.deltaTime * velocidad * moverVertical);
+            transform.Translate(Vector3.up * Time.deltaTime * _velocidad * moverVertical);
         }
 
         // Control al llegar a un borde
         // Eje X
-        if (transform.position.x >= 7.5 && horizontalInput < 0)
+        if (transform.position.x >= 7.5 && _horizontalInput < 0)
         {
-            transform.Translate(Vector3.right * Time.deltaTime * velocidad * moverHorizontal);
+            transform.Translate(Vector3.right * Time.deltaTime * _velocidad * moverHorizontal);
         }
-        if (transform.position.x <= -7.5 && horizontalInput > 0)
+        if (transform.position.x <= -7.5 && _horizontalInput > 0)
         {
-            transform.Translate(Vector3.right * Time.deltaTime * velocidad * moverHorizontal);
+            transform.Translate(Vector3.right * Time.deltaTime * _velocidad * moverHorizontal);
         }        
         // Eje Y
-        if (transform.position.y >= 1.25f && verticalInput < 0)
+        if (transform.position.y >= 1.25f && _verticalInput < 0)
         {
-            transform.Translate(Vector3.up * Time.deltaTime * velocidad * moverVertical);
+            transform.Translate(Vector3.up * Time.deltaTime * _velocidad * moverVertical);
         }
-        if (transform.position.y <= -4.25f && verticalInput > 0)
+        if (transform.position.y <= -4.25f && _verticalInput > 0)
         {
-            transform.Translate(Vector3.up * Time.deltaTime * velocidad * moverVertical);
+            transform.Translate(Vector3.up * Time.deltaTime * _velocidad * moverVertical);
         }
     }
 }
